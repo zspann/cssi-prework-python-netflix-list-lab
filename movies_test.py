142c1bb9b0d85a4aecca70f502b9c622eb79c542
dfenjves <fenjves.daniel@gmail.com>
2016-03-27 18:22:29 -0400

    fixed test

--- movies_test.py
+++ movies_test.py
@@ -20,7 +20,7 @@ class TestMovies(unittest.TestCase):
     
     def test_view_queue(self):
         movie_list = ["The Shawshank Redemption", "Wall-E", "The Dark Knight Rises", "George of the Jungle", "Fargo"]
-        self.assertEqual(view_queue(movie_list), "1. The Shawshank Redemption\n2. Wall-E\n3. The Dark Knight Rises\n4. George of the Jungle\n5. Fargo")
+        self.assertEqual(view_queue(movie_list), "1. The Shawshank Redemption\n2. Wall-E\n3. The Dark Knight Rises\n4. George of the Jungle\n5. Fargo\n")
 
 if __name__ == '__main__':
     unittest.main()
